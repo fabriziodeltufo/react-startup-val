a79f6d5ca0c819bddbc9bc1971fbe466a28a9e29
FABRIZIO DEL TUFO <fabrizio.deltufo@gmail.com>
2026-06-06 20:39:05 +0200

    add authcontext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,56 @@
+import React, { createContext, useContext, usestate, useEffect } from 'react';
+import { supabase } from '../services/supabase';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [session, setSession] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Check active sessions and sets the user
+        supabase.auth.getSession().then(({ data: { session } }) => {
+
+            setSession(session);
+            setUser(session?.user ?? null);
+            setLoading(false);
+        });
+
+        // Listen for changes on auth state (logged in, signed out, etc.)
+        const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+            setUser(session?.user ?? null);
+            setLoading(false);
+        });
+
+        return () => subscription.unsubscribe();
+    }, []);
+
+    const value = {
+        session,
+        user,
+        loading,
+        signUp: () => { },
+        signIn: () => { },
+        signOut: () => { },
+    };
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+
+
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error('useAuth must be used within an AuthProvider');
+
+    }
+
+    return context;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { GlobalProvider } from './context/globalContext.jsx'
+import { GlobalProvider } from './context/globalContext'
+import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GlobalProvider>
-      <App />
-    </GlobalProvider>
+    <AuthProvider>
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
+    </AuthProvider>
   </StrictMode>,
 )
